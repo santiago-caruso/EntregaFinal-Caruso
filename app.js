54a538022a6ee5d90c05a4094dfecf49e566331c
santicaruso <santiagocarusoo@gmail.com>
2023-10-22 17:31:41 -0300

    proyecto final

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-
+//Molde productos
 class Producto {
     constructor(id, nombre, precio, categoria, imagen) {
         this.id = id;
@@ -10,54 +10,70 @@ class Producto {
     }
 }
 
+//Base de datos del e commerce, todos los productos de nuestro catalogo
 class BaseDeDatos {
+    
     constructor() {
-
-        this.productos = [];
-            this.agregarRegistro(1, "Macbook Pro", 1499, "Tecnologia", "img/macbook.jpg");
-            this.agregarRegistro(2, "Mouse Apple", 299, "Tecnologia", "img/mouse.jpg");
-            this.agregarRegistro(3, "Iphone 14 Pro Max", 1299, "Tecnologia", "img/iphone.jpg");                 
-            this.agregarRegistro(5, "Apple Watch", 199, "Tecnologia", ".img/smart-watch.jpg");
-            this.agregarRegistro(6, "Joystick Ps4", 59, "Tecnologia", ".img/joystick.jpg");
-            this.agregarRegistro(6, "Monitor Apple", 1559, "Tecnologia", ".img/monitor.jpg");
+        
+      //Array del catalogo
+      this.productos = [];
+        //llamamos a la funcion asincronica
+        this.cargarRegistros();
     }
 
-    agregarRegistro(id, nombre, precio, categoria, imagen) {
-        const producto = new Producto(id, nombre, precio, categoria, imagen);
-        this.productos.push(producto);
+    //Funcion asincronica para cargar los productos desde un JSON
+    async cargarRegistros(){
+      const resultado = await fetch("./json/productos.json");
+      this.productos = await resultado.json();
+      cargarProductos(this.productos);
     }
 
+    //catalogo de productos devuelto
     traerRegistros() {
         return this.productos;
     }
 
+    //nos da producto segun su id
     registroPorId(id) {
         return this.productos.find((producto) => producto.id === id);
     }
 
-        registrosPorNombre(palabra) {
+    //nos da producto segun nombre
+    registrosPorNombre(palabra) {
             return this.productos.filter((producto) =>producto.nombre.toLowerCase().includes(palabra.toLowerCase()));
     }
+
+    //nos da producto segun la categoria
+    registrosPorCategoria(categoria){
+          return this.productos.filter((producto) => producto.categoria == categoria)
+        }
 }
 
+//CLase para manipular el carrito
 class Carrito {
-    constructor() {
-        const carritoStorage = JSON.parse(localStorage.getItem("carrito"));
+    constructor() {  
+      //storage 
+      const carritoStorage = JSON.parse(localStorage.getItem("carrito"));
 
+        //array donde van los productos almacenados
         this.carrito = carritoStorage || [];
         this.total = 0; 
         this.cantidadProductos = 0; 
     
+        //llamo la funcion para aplicar lo que haya en el storage si es que hay
         this.listar();
     }
 
+    //para saber si el producto se ebcuentra en el carrito
     estaEnCarrito({ id }) {
     return this.carrito.find((producto) => producto.id === id);
 }
 
+  //agregamos al carrito
   agregar(producto) {
     const productoEnCarrito = this.estaEnCarrito(producto);
   
+    //si no esta en el carrito le mado push sino me agrego 1 de cantidad
     if (!productoEnCarrito) {
       this.carrito.push({ ...producto, cantidad: 1 });
     } else {
@@ -68,6 +84,7 @@ class Carrito {
     this.listar();
 }
 
+  //quitar del carrito
   quitar(id) {
 
     const indice = this.carrito.findIndex((producto) => producto.id === id);
@@ -82,6 +99,7 @@ class Carrito {
     this.listar();
   }
 
+  //vaciar el carrito
   vaciar(){
     this.total=0;
     this.cantidadProductos=0;
@@ -91,13 +109,14 @@ class Carrito {
 
   }
 
-
+    //renderiza los productos en el html
     listar() {
 
         this.total = 0;
         this.cantidadProductos = 0;
         divCarrito.innerHTML = "";
 
+    //recorro los productos y se renderizan al html
     for (const producto of this.carrito) {
         divCarrito.innerHTML += `
         <div class="productoCarrito">
@@ -112,6 +131,15 @@ class Carrito {
       this.cantidadProductos += producto.cantidad;
   }
 
+  if(this.cantidadProductos > 0){
+    //boton comprar visible
+    botonComprar.style.display = "block";
+  
+  }else{
+    //boton comprar oculto
+    botonComprar.style.display = "none";
+  }
+
     const botonesQuitar = document.querySelectorAll(".btnQuitar");
 
     for (const boton of botonesQuitar) {
@@ -123,13 +151,13 @@ class Carrito {
       });
   }
 
+  //actualizo contadores del html
   spanCantidadProductos.innerText = this.cantidadProductos;
     spanTotalCarrito.innerText = this.total;
   }
 }
 
-const bd = new BaseDeDatos();
-
+//elementos
 const spanCantidadProductos = document.querySelector("#cantidadProductos");
 const spanTotalCarrito = document.querySelector("#totalCarrito");
 const divProductos = document.querySelector("#productos");
@@ -137,14 +165,34 @@ const divCarrito = document.querySelector("#carrito");
 const inputBuscar = document.querySelector("#inputBuscar");
 const botonCarrito = document.querySelector("section h1");
 const botonComprar = document.querySelector("#botonComprar");
+const botonesCategorias = document.querySelectorAll(".btnCategoria");
 
+//se instacia la base de datos
+const bd = new BaseDeDatos();
 
+//se instancia el carrito
 const carrito = new Carrito();
 
+botonesCategorias.forEach((boton) => {
+  boton.addEventListener("click" , () => {
+    const categoria = boton.dataset.categoria;
+    //Saco boton seleccionado 
+    const botonSeleccionado = document.querySelector(".seleccionado");
+    botonSeleccionado.classList.remove("seleccionado");
+    //Se lo agrego al nuevo
+    boton.classList.add("seleccionado");
+    if(categoria == "Todos"){
+      cargarProductos(bd.traerRegistros());
+    }else{
+      cargarProductos(bd.registrosPorCategoria(categoria));
+    }
+  })
+});
 
+//mostramos el catalogo de la base de datos apenas cargar la pagina
 cargarProductos(bd.traerRegistros());
 
-
+//funcion que renderiza productos del catalogo 
 function cargarProductos(productos) {
 
   divProductos.innerHTML = "";
@@ -162,10 +210,11 @@ function cargarProductos(productos) {
     `;
   }
 
-
+    //lista con todos los bootnes que haya en nuestro catalogo
   const botonesAgregar = document.querySelectorAll(".btnAgregar");
 
 
+  //recorremos boton de cada producto y le damos evento click a cada uno
   for (const boton of botonesAgregar) {
     boton.addEventListener("click", (event) => {
 
@@ -175,10 +224,21 @@ function cargarProductos(productos) {
       const producto = bd.registroPorId(idProducto);
 
       carrito.agregar(producto);
+
+      //alert de toastify
+      Toastify({
+        text: `Se ha añadido ${producto.nombre} al carrito`,
+        gravity: "bottom",
+        position: "center",
+        style: {
+          background: "linear-gradient(to right, #00b09b, #5e7bef)",
+        },
+      }).showToast();
     });
   }
 }
 
+//buscador
 inputBuscar.addEventListener("input", (event) => {
   event.preventDefault();
   const palabra = inputBuscar.value;
@@ -186,13 +246,17 @@ inputBuscar.addEventListener("input", (event) => {
   cargarProductos(productos);
 });
 
+//toggle que muestra u oculta el carrito
 botonCarrito.addEventListener("click", (event) => {
   document.querySelector("section").classList.toggle("ocultar");
 });
 
+//boton comprar
 botonComprar.addEventListener("click", (event) =>{
   event.preventDefault();
   carrito.vaciar();
+
+  //alert de sweet alert
   Swal.fire({
     title: '¿Estás seguro que tenés lo que necesitas?',
     text: "Se comprarán todos los objectos del carrito!",
